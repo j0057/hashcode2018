2d5dd5b2bd4bd191f7a36fd4103a18846065a1a2
Joost Molenaar <j.j.molenaar@gmail.com>
2018-03-01 19:23:07 +0100

    Parse the input file

--- HashCode2018/Program.cs
+++ HashCode2018/Program.cs
@@ -1,16 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HashCode2018
 {
+    public class Ride
+    {
+        public Tuple<int, int> start;
+        public Tuple<int, int> finish;
+        public int earliest;
+        public int latest;
+
+        public static Ride Parse(string line)
+        {
+            var nums = line.Split(' ').Select(int.Parse).ToArray();
+            return new Ride
+            {
+                start = Tuple.Create(nums[0], nums[1]),
+                finish = Tuple.Create(nums[2], nums[3]),
+                earliest = nums[4],
+                latest = nums[5]
+            };
+        }
+    }
+
     class Program
     {
+        static void ParseFirst(string line, out int rows, out int cols, out int vehicles, out int rides, out int bonus, out int steps)
+        {
+            var coords = line.Split(' ').Select(int.Parse).ToArray();
+            rows = coords[0];
+            cols = coords[1];
+            vehicles = coords[2];
+            rides = coords[3];
+            bonus = coords[4];
+            steps = coords[5];
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            var lines = File.ReadAllLines(args[0]);
+
+            ParseFirst(lines[0], out var rows, out var cols, out var vehicles, out var rideCount, out var bonus, out var steps);
+
+            var rides = lines.Skip(1).Select(Ride.Parse).ToList();
+
             Console.ReadLine();
         }
     }
